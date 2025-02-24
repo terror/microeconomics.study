3433650eb2f2c82bd3c1bde16f8e2965cf45cd0e
Liam <liam@scalzulli.com>
2025-02-24 00:32:02 -0500

    Deploy to github pages

--- justfile
+++ justfile
@@ -5,8 +5,12 @@ export EDITOR := 'nvim'
 default:
   just --list
 
+deploy:
+  bun run build && bunx gh-pages -d dist
+
 dev:
   bun run dev
 
 fmt:
   prettier --write .
+
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/microeconomics.study',
   plugins: [react()],
   resolve: {
     alias: {
